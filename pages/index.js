dfcf1fa9955d993bb48fcdf5163532516e8d2c3d
watmot <tomwatson840@gmail.com>
2022-01-26 22:10:27 +0000

    Implement base preloader functionality

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,21 @@
 import Center from '../components/Center';
 import Sidebar from '../components/Sidebar';
+import { useSession } from 'next-auth/react';
 
 export default function Home() {
-  return (
-    <div className="bg-black h-screen overflow-hidden">
-      <main className="flex">
-        <Sidebar />
-        <Center />
-      </main>
-      <div>{/* {Player} */}</div>
-    </div>
-  );
+  const { data: session, status } = useSession();
+
+  if (session) {
+    return (
+      <div className="bg-black h-screen overflow-hidden">
+        <main className="flex">
+          <Sidebar />
+          <Center />
+        </main>
+        <div>{/* {Player} */}</div>
+      </div>
+    );
+  }
+
+  return <div>Loading...</div>;
 }
