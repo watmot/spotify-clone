7fca02cd232ba5b2a07796ec677c5059430f7db3
watmot <tomwatson840@gmail.com>
2022-01-26 22:23:45 +0000

    Add icon to logout button

--- components/Center/StickyHeader/Profile/Menu/index.js
+++ components/Center/StickyHeader/Profile/Menu/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { signOut, useSession } from 'next-auth/react';
+import { LogoutIcon } from '@heroicons/react/outline';
 
 const Menu = () => {
   return (
     <div className="absolute flex flex-col items-start right-0 top-12 w-[12.5rem] bg-[#282828] rounded-md text-sm">
-      <button className="p-3 text-white" onClick={signOut}>
+      <button
+        className="flex justify-between w-full p-3 text-white"
+        onClick={signOut}
+      >
         <p>Log out</p>
+        <LogoutIcon className="w-5 h-5" />
       </button>
     </div>
   );
