dccfc24c89f4c4ec326b2d4cfef2037e833e9549
Tom Watson <tomwatson840@gmail.com>
2022-01-27 10:42:57 +0000

    Update styling for sidebar

--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -38,36 +38,36 @@ function Sidebar() {
   }, [session, setPlaylists, setActivePlaylist, spotifyApi]);
 
   return (
-    <div className="grow-0 min-w-[17.5rem] text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen">
+    <div className="grow-0 min-w-[17.5rem] text-gray-300 p-5 text-sm border-r border-black overflow-y-scroll scrollbar-hide h-screen">
       <div className="space-y-4">
-        <button className="flex items-center space-x-2 hover:text-white">
-          <HomeIcon className="h-5 w-5" />
-          <p>Home</p>
-        </button>
-        <button className="flex items-center space-x-2 hover:text-white">
-          <SearchIcon className="h-5 w-5" />
-          <p>Search</p>
-        </button>
-        <button className="flex items-center space-x-2 hover:text-white">
-          <LibraryIcon className="h-5 w-5" />
-          <p>Your Library</p>
-        </button>
-        <hr className="border-t-[0.1px] border-gray-900" />
+        <div className="space-y-5">
+          <button className="flex items-center space-x-4 hover:text-white">
+            <HomeIcon className="h-5 w-5" />
+            <p className="font-bold">Home</p>
+          </button>
+          <button className="flex items-center space-x-4 hover:text-white">
+            <SearchIcon className="h-5 w-5" />
+            <p className="font-bold">Search</p>
+          </button>
+          <button className="flex items-center space-x-4 hover:text-white pb-4">
+            <LibraryIcon className="h-5 w-5" />
+            <p className="font-bold">Your Library</p>
+          </button>
+          <button className="flex items-center space-x-4 hover:text-white">
+            <PlusCircleIcon className="h-5 w-5" />
+            <p className="font-bold">Create Playlist</p>
+          </button>
+          <button className="flex items-center space-x-4 hover:text-white">
+            <HeartIcon className="h-5 w-5" />
+            <p className="font-bold">Liked Songs</p>
+          </button>
+          <button className="flex items-center space-x-4 hover:text-white">
+            <RssIcon className="h-5 w-5" />
+            <p className="font-bold">Your Episodes</p>
+          </button>
+        </div>
 
-        <button className="flex items-center space-x-2 hover:text-white">
-          <PlusCircleIcon className="h-5 w-5" />
-          <p>Create Playlist</p>
-        </button>
-        <button className="flex items-center space-x-2 hover:text-white">
-          <HeartIcon className="h-5 w-5" />
-          <p>Liked Songs</p>
-        </button>
-        <button className="flex items-center space-x-2 hover:text-white">
-          <RssIcon className="h-5 w-5" />
-          <p>Your Episodes</p>
-        </button>
         <hr className="border-t-[0.1px] border-gray-900" />
-
         {/* PLAYLISTS */}
         {playlists &&
           playlists.map((playlist) => (
